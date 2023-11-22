eee5ad3be3f59c6e9c878437c73322f0ba98a6e7
Darshan <darshanv487@gmail.com>
2023-11-22 18:14:50 +0530

    created get by author

--- contollers/createBlogControllers.js
+++ contollers/createBlogControllers.js
@@ -1,30 +1,122 @@
-const Blog=require("../models/Blogs")
-const postBlog=async(req,res)=>{
-    try {
-        let user=req.user
-      const newBlog=await  Blog.create({
-            title:req.body.title,
-            snippet:req.body.snippet,
-            description:req.body.description,
-            image:req.body.image,
-            author:user._id
-        })
-        res.status(201).json({
-            status:"success",
-            data:{
-                newBlog
-            }
+const Blog = require("../models/Blogs");
+const User = require("../models/User");
+const postBlog = async (req, res) => {
+  try {
+    let user = req.user;
+    const newBlog = await Blog.create({
+      title: req.body.title,
+      snippet: req.body.snippet,
+      description: req.body.description,
+      image: req.body.image,
+      author: user._id,
+    });
+    res.status(201).json({
+      status: "success",
+      data: {
+        newBlog,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
 
-        })
-        
-    } catch (error) {
-        res.status(400).json({
-            status:"fail",
-            message:error.message
-        })
-    }
-}
+const getBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res.status(200).json({
+      status: "success",
+      data: {
+        blogs,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
 
-module.exports={
-    postBlog
-}
+const getByAuthor = async (req, res) => {
+  try {
+    let user = req.user;
+    const blogs = await Blog.find({ author: user._id });
+    res.status(201).json({
+      status: "success",
+      data: {
+        blogs,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+const getBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    res.status(200).json({
+      status: "success",
+      data: {
+        blog,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+const updateBlog = async (req, res) => {
+  try {
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        updatedBlog,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+const deleteBlog = async (req, res) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  postBlog,
+  getBlogs,
+  getBlog,
+  updateBlog,
+  deleteBlog,
+  getByAuthor
+};
--- routes/BlogRouts.js
+++ routes/BlogRouts.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
-const { postBlog } = require("../contollers/createBlogControllers");
+const { postBlog, getBlogs, getBlog, updateBlog, deleteBlog, getByAuthor} = require("../contollers/createBlogControllers");
+
 const auth = require("../middlewares/authMiddleWare");
 
 router.post("/",auth,postBlog)
-// router.get("/",auth,getBlogs)
-// router.get("/:id",auth,getBlog)
-// router.patch("/:id",auth,updateBlog)
-// router.delete("/:id",auth,deleteBlog)
+router.get("/",auth,getBlogs)
+router.get("/author",auth,getByAuthor)
+router.get("/:id",auth,getBlog)
+router.patch("/:id",auth,updateBlog)
+router.delete("/:id",auth,deleteBlog)
 
 module.exports = router;
