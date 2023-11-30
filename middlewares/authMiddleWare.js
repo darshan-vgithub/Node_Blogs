b9044cbaa36d17031ea5a4da0eb0e7cef9f91c26
Darshan <darshanv487@gmail.com>
2023-11-30 18:04:03 +0530

    implemented error handling

--- middlewares/authMiddleWare.js
+++ middlewares/authMiddleWare.js
@@ -1,43 +1,45 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const auth = async (req, res, next) => {
-  try {
-    let testToken = req.headers.authorization;
-    let token;
-    if (testToken && testToken.startsWith("Bearer")) {
-      token = testToken.split(" ")[1];
-    }
-    if (!token) {
-      return res.status(401).json({
-        status: "fail",
-        message: "Try logging in to access",
-      });
-    }
-    const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
-    const user = await User.findById(decodedToken.id);
-    if (!user) {
-      return res.status(401).json({
-        status: "fail",
-        message: "user no longer exists",
-      });
-    }
-    req.user = user;
-    next();
-  } catch (error) {
-    res.status(401).json({
-      status: "fail",
-      message: error.message,
-    });
+const asyncErrorhandler = require("../utils/asyncErrorHandler");
+const CustomError = require("../utils/CustomError");
+const auth = asyncErrorhandler(async (req, res, next) => {
+  let testToken = req.headers.authorization;
+  let token;
+  if (testToken && testToken.startsWith("Bearer")) {
+    token = testToken.split(" ")[1];
   }
-};
+  if (!token) {
+    // return res.status(401).json({
+    //   status: "fail",
+    //   message: "Try logging in to access",
+    // });
+    const err = new CustomError(400, "Try logging in to access");
+    next(err);
+  }
+  const decodedToken = await jwt.verify(token, process.env.JWT_SECRET);
+  const user = await User.findById(decodedToken.id);
+  if (!user) {
+    // return res.status(401).json({
+    //   status: "fail",
+    //   message: "user no longer exists",
+    // });
+    const err = new CustomError(400, "user no longer exists");
+    next(err);
+  }
+  req.user = user;
+  next();
+});
 
 const verifyRole = (role) => {
   return (req, res, next) => {
     if (!role.includes(req.user.role)) {
-      return res.status(400).json({
-        status: "fail",
-        message: "you are not authorized",
-      });
+      // return res.status(400).json({
+      //   status: "fail",
+      //   message: "you are not authorized",
+      // });
+
+      const err = new CustomError(400, "you are not authorized");
+      next(err);
     }
     next();
   };
